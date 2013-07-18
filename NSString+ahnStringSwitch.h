d49fdb3fe508de6753b05057fbadba8336f44cb4
Naglbagl <AlexanderHnagl@gmail.com>
2013-07-18 09:24:45 -0400

    Create NSString+ahnStringSwitch.h

---
+++ NSString+ahnStringSwitch.h
@@ -0,0 +1,23 @@
+//
+//  NSString+StringSwitch.h
+//  stringSwitch
+//
+//  Created by Alexander Nagl on 7/17/13.
+//  Copyright (c) 2013 Alexander Nagl. All rights reserved.
+/*
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (StringSwitch)
+
+-(bool)switchWithCases:(NSArray *)cases withStatements:(void(^)(void))statements,...;
+
+@end
